4d61cc5110f9745b5ce2c0e966a336fcb2c16c48
Franky Van Liedekerke <franky@e-dynamics.be>
2025-12-07 18:43:14 +0100

    remove small comments

--- src/ftable.js
+++ src/ftable.js
@@ -4314,7 +4314,6 @@ class FTable extends FTableEventEmitter {
     reload(preserveSelection = false) {
         this.clearListCache();
         if (preserveSelection) {
-            // Bewaar huidige selecties
             this.preservedSelections = new Set(this.state.selectedRecords);
         } else {
             this.state.selectedRecords.clear();
@@ -4322,7 +4321,6 @@ class FTable extends FTableEventEmitter {
 
         return this.load().then(() => {
             if (preserveSelection && this.preservedSelections) {
-                // Herstel selecties na reload
                 this.restoreSelections();
                 this.preservedSelections = null;
             }
@@ -4336,11 +4334,6 @@ class FTable extends FTableEventEmitter {
         }
     }
 
-    preserveCurrentSelections() {
-        this.preservedSelections = new Set(this.state.selectedRecords);
-        return this;
-    }
-
     restoreSelections() {
         if (!this.preservedSelections) return;
 
@@ -4351,8 +4344,6 @@ class FTable extends FTableEventEmitter {
                 this.selectRow(row);
             }
         });
-
-        this.emit('selectionChanged', { selectedRows: this.getSelectedRows() });
         return this;
     }
 
