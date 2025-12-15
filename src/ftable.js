37ed94257dd276f5ca24b3ad6e01d1c884e185ed
Franky Van Liedekerke <franky@e-dynamics.be>
2025-12-15 12:59:15 +0100

    fix value for textarea

--- src/ftable.js
+++ src/ftable.js
@@ -1361,9 +1361,9 @@ class FTableFormBuilder {
 
         const textarea = FTableDOMHelper.create('textarea', {
             className: field.inputClass || null,
-            attributes: attributes,
-            value: value
+            attributes: attributes
         });
+        textarea.value = value;
         return textarea;
     }
 
